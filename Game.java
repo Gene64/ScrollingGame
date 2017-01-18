6c47c2bc62f8dbdfd3ed1da2bc7503ba58c1274d
Gene Pressinger <genepressinger@yahoo.com>
2017-01-18 15:57:39 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -36,85 +36,118 @@ public class Game
   }
   
   public int userRowLocation() {
-	  for (int i = 0; i < 5; i++) {
-		  Location loc = new Location(i, 0);
-		  
-		  if (grid.getImage(loc).equals("user.gif"))
-			  return i;
-	  }
-	  return 0;
+   for (int i = 0; i < 5; i++) {
+    Location loc = new Location(i, 0);
+    
+    if (grid.getImage(loc).equals("user.gif"))
+     return i;
+   }
+   return 0;
   }
   
   public void handleKeyPress()
   {
-	  int key = grid.checkLastKeyPressed();
-	  Location originalLoc = new Location(userRowLocation(), 0);
-	  
-	  if (key == 38) {
-		  if (originalLoc.getRow() == 0)
-			  return;
-		  
-		  Location newLoc = new Location(originalLoc.getRow() - 1, 0);
-		  grid.setImage(newLoc, "user.gif");
-		  grid.setImage(originalLoc, "");
-		  
-	  } else if (key == 40) {
-		  if (originalLoc.getRow() == 4)
-			  return;
-		  
-		  Location newLoc = new Location(originalLoc.getRow() + 1, 0);
-		  grid.setImage(newLoc, "user.gif");
-		  grid.setImage(originalLoc, "");
-	  }
+   int key = grid.checkLastKeyPressed();
+   Location originalLoc = new Location(userRowLocation(), 0);
+   
+   if (key == 38) {
+    if (originalLoc.getRow() == 0)
+     return;
+    
+    Location newLoc = new Location(originalLoc.getRow() - 1, 0);
+    
+    if (grid.getImage(newLoc) != null)
+      handleCollision(newLoc);
+    
+    grid.setImage(newLoc, "user.gif");
+    grid.setImage(originalLoc, "");
+    
+   } else if (key == 40) {
+    if (originalLoc.getRow() == 4)
+     return;
+    
+    Location newLoc = new Location(originalLoc.getRow() + 1, 0);
+    
+    if (grid.getImage(newLoc) != null)
+      handleCollision(newLoc);
+    
+    grid.setImage(newLoc, "user.gif");
+    grid.setImage(originalLoc, "");
+   }
   }
   
   public void populateRightEdge()
   {
-	Location loc = new Location((int)(Math.random() * 5), 9);
-	  
-	if (Math.random() < 0.5) {
-		grid.setImage(loc, "avoid.gif");
-	} else {
-		grid.setImage(loc, "get.gif");
-	}
+ Location loc = new Location((int)(Math.random() * 5), 9);
+   
+ if (Math.random() < 0.5) {
+  grid.setImage(loc, "avoid.gif");
+ } else {
+  grid.setImage(loc, "get.gif");
+ }
   }
   
   public void scrollLeft()
   {
-	  String currentImg;
-	  for (int x = 1; x < 9; x++) {
-		  for (int y = 0; y < 4; y++) {
-			  Location loc = new Location(y, x);
-			  currentImg = grid.getImage(loc);
-			  Location loc1 = new Location(y, x - 1);
-			  if (currentImage != null && !currentImg.equals("user.gif"))
-				  grid.setImage(loc1, currentImg);
-		  }
-	  }
+    if (grid.getImage(new Location(userRowLocation(), 1)) != null)
+      handleCollision(new Location(userRowLocation(), 1));
+      
+   // Columns
+   for (int x = 1; x < 10; x++) {
+    // Rows
+    for (int y = 0; y < 5; y++) {
+     Location currentLoc = new Location(y, x);
+     Location moveLeftLoc = new Location(y, x - 1);
+     
+      if (grid.getImage(currentLoc) != null) {
+       grid.setImage(moveLeftLoc, grid.getImage(currentLoc));
+       grid.setImage(currentLoc, "");
+     }
+    }
+   }
+   //removeFirstColumnImages();
+  }
+  
+  public void removeFirstColumnImages() {
+    for (int i = 0; i < 5; i++) {
+      if (grid.getImage(new Location(i, 0)) != null && grid.getImage(new Location(i, 0)).equals("user.gif"))
+        grid.setImage(new Location(i, 0), null);
+    }
   }
   
   public void handleCollision(Location loc)
   {
+    if (grid.getImage(loc).equals("get.gif")) {
+      timesGet++;
+    }
+    else if (grid.getImage(loc).equals("avoid.gif")) {
+      timesAvoid++;
+    }
+    
+    grid.setImage(loc, null);
   }
   
   public int getScore()
   {
-    return 0;
+    return timesGet - timesAvoid;
   }
   
   public void updateTitle()
   {
-    grid.setTitle("Scrolling Game | Current Score: " + getScore());
+    if (timesGet < timesAvoid)
+      grid.setTitle("Game Over - Try Again");
+    else
+      grid.setTitle("Scrolling Game | Current Score: " + getScore());
   }
   
   public boolean isGameOver()
   {
-    return false;
+    return timesGet < timesAvoid;
   }
   
   public static void main(String[] args)
   {
-	  Game game = new Game();
-	  game.play();
+   Game game = new Game();
+   game.play();
   }
 }
